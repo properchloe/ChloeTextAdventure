0a9977892bfe01015e15e84cf50b3bd033fbd1f7
properchloe <cmonson.f21@warren-wilson.edu>
2021-12-01 15:32:31 -0500

    v1.0 - Beginning the framework for the classes needed.

--- AdventureClasses.py
+++ AdventureClasses.py
@@ -1,1 +1,27 @@
-# starting out!
+class Character():
+    """ Class to hold stats for any living being in the game. """
+
+    def __init__(self):
+        #
+
+class Player(Character):
+    """ Class to hold information for the player character. """
+
+    #
+
+class Item():
+    """ Class to hold information for interactable objects in-game. """
+
+    def __init__(self):
+        #
+
+class Weapon(Item):
+    """ Class to hold information about weapons for combat. """
+
+    #
+
+class Container():
+    """ Class to hold information about containers that hold items. """
+
+    def __init__(self):
+        #
