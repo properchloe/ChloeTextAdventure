7594b3104218746d82d5064e6eeb9913bef0b035
properchloe <cmonson.f21@warren-wilson.edu>
2021-12-01 15:40:09 -0500

    v1.1 - Framework cont'd

--- AdventureClasses.py
+++ AdventureClasses.py
@@ -1,13 +1,16 @@
 class Character():
     """ Class to hold stats for any living being in the game. """
 
-    def __init__(self):
-        #
+    def __init__(self, name, inventory = []): #can you do default values in class inits?
+        self.name = name
+        self.inventory = inventory
+
 
 class Player(Character):
     """ Class to hold information for the player character. """
 
     #
+
 
 class Item():
     """ Class to hold information for interactable objects in-game. """
@@ -15,10 +18,12 @@ class Item():
     def __init__(self):
         #
 
+
 class Weapon(Item):
     """ Class to hold information about weapons for combat. """
 
     #
+
 
 class Container():
     """ Class to hold information about containers that hold items. """
